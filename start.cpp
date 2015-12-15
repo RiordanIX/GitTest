76d7ca5b1329354e494b2dc935a690c6fcbf5717
RiordanIX <majorjctaylor@gmail.com>
2015-12-15 01:18:43 -0500

    Added a line and extra semi-colon (doh!) Hopefully this shows up on Github.

--- start.cpp
+++ start.cpp
@@ -1,7 +1,8 @@
 #include <stdio>
 
 int main() {
-	std:cout << "Hello world" << std:endl;
-
+	std::cout << "Hello world" << std::endl;
+	std::cout << "Where are you?" << std::endl;
+	
 	return 0;
 }
