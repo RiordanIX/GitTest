fec048788b2388607a21f15f13bf4c4a2682e83c
RiordanIX <majorjctaylor@gmail.com>
2015-12-15 21:05:45 -0500

    Added functionality again, opened up an image and showed it to the screen.

--- start.cpp
+++ start.cpp
@@ -4,55 +4,118 @@
 using std::cout;
 using std::endl;
 
+// startup SDL and create window
+bool init();
+
+// Loads media
+bool loadMedia();
+
+// Frees media and shuts down SDL
+void close();
+
+
+
+// global variables to be used in functions 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main(int argc, char* args[]) {
-	// The window we're rendering to.
-	SDL_Window* window = nullptr;
+// The window we're rendering to
+SDL_Window* gWindow = nullptr;
 
-	// Surface contained by the window.
-	SDL_Surface* screenSurface = nullptr;
+// The surface contained by the window
+SDL_Surface* gScreenSurface = nullptr;
 
-	// Initialize SDL
+// The image we will load and show on the screen
+SDL_Surface* gHelloWorld = nullptr;
 
-	if (SDL_Init( SDL_INIT_VIDEO) < 0 ) {
-		cout << "SDL could not initialize! SDL_Error: " ;
-		cout << SDL_GetError();
-		cout << endl;
+
+
+int main ( int argc, char* args[] ) {
+	//Startup SDL and create window
+	if ( !init() ) {
+		cout << "Failed to initialize!" << endl;
 	}
 	else {
-		// create window
-		window = SDL_CreateWindow( "Testing", SDL_WINDOWPOS_UNDEFINED,
-			SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT,
-			SDL_WINDOW_SHOWN );
-		if (window == NULL) {
-			cout << "Window could not be created! SDL_Error: ";
-			cout << SDL_GetError() << endl;
+		if ( !loadMedia()) {
+			cout << "Failed to load media!" << endl;
 		}
 		else {
-			// Get Window surface
-			screenSurface = SDL_GetWindowSurface(window);
-
-			// Fill the surface with white
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(
-				screenSurface->format, 0xFF,0xFF,0xFF) );
-			
-			//Update the surface
-			SDL_UpdateWindowSurface( window );
-
-
-			// Wait two seconds
-			SDL_Delay( 2000 );
+			// Apply the image
+			SDL_BlitSurface ( gHelloWorld, NULL, gScreenSurface,
+				NULL );
 		}
 	}
 
-	// Destroy the window
-	SDL_DestroyWindow( window);
+	// Update the surface (draw to screen)
+	SDL_UpdateWindowSurface ( gWindow );
 
-	std::cout << "It worked!" << std::endl;
+	// Wait a few seconds
+	SDL_Delay ( 5000 );
+
+	// end the session and free resources
+	close();
+	return 0;
+}
+
+
+bool init() {
+	// Initialization flag
+	bool success = true;
+
+	// Initialize SDL
+	if ( SDL_Init ( SDL_INIT_VIDEO ) < 0 ) {
+		cout << "SDL could not initialize! SDL_Error: ";
+		cout << SDL_GetError() << endl;
+		success = false;
+	}
+	else {
+		// Create window
+		gWindow = SDL_CreateWindow( "Testing", SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT,
+			SDL_WINDOW_SHOWN );
+		if ( gWindow == nullptr ) {
+			cout << "Window could not be created! SDL_Error: ";
+			cout << SDL_GetError();
+			success = false;
+		}
+		else {
+			// Get window surface
+			gScreenSurface = SDL_GetWindowSurface( gWindow );
+		}
+	}
+
+	return success;
+}
+
+
+// Loading media from image files
+bool loadMedia() {
+	// Loading success flag
+	bool success = true;
+
+	// Load splash image
+	gHelloWorld = SDL_LoadBMP( "media/preview.bmp" );
+	if ( gHelloWorld == nullptr) {
+		cout << "Unable to load image media/preview.png" << endl;
+		success = false;
+	}
+
+	return success;
+}
+
+
+
+// release memory and end session
+void close() {
+	// Deallocate surface
+	SDL_FreeSurface ( gHelloWorld );
+	gHelloWorld = nullptr;
+
+	// Destroy window
+	SDL_DestroyWindow ( gWindow );
+	gWindow = nullptr;
 
 	// Quit SDL subsystems
 	SDL_Quit();
-	return 0;
 }
+
